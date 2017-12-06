188fbcecdf6734d3e2edfc902dc0d384e54df768
Pascal Pfeiffer <passiman@live.de>
2017-12-06 22:59:49 +0100

    Hindernisgenerator implementiert

--- THM_PGP_Projekt/MainSideScroller.cpp
+++ THM_PGP_Projekt/MainSideScroller.cpp
@@ -1,10 +1,25 @@
 
-#include <stdio.h>
+#include <stdio.h>														// bringt Ein/Ausgabefunktionen mit
 #include <algorithm>
 #include <ctype.h>
-#include <Windows.h>
-#include <stdlib.h>
+#include <Windows.h>													// benötigt um die Konsole zu leern
+#include <stdlib.h>														// bringt Standardfunktionen mit
+#include <time.h>														// für Zufallszahlen benötigt
 #include "scorelist.h"
+#include "ObstacleGenerator.h"
+
+/*	init
+	nimmt für das Programm
+	benötigte initialiserungen vor
+*/
+void init() {
+	srand(time(NULL)*clock());											// Initialisiert den Zufallszahlengenerator
+
+}
+
+
+
 int main() {
+	init();
 	return 1;
 }
